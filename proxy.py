cde0f14ccd795e0ddc436b0ef5a81a040801940f
alexwroble <56139254+alexwroble@users.noreply.github.com>
2025-01-03 08:56:40 -0500

    Added functions for processing/modifying data forwarded through proxy between client/server

--- proxy.py
+++ proxy.py
@@ -43,7 +43,8 @@ def hexdump(src, length=16, show=True):
 # conn: connection -- socket object
 def recv_from(conn):
   buff = b''
-  conn.settimeout(5) # timeout exception thrown when socket operation takes more than 5 seconds
+  # timeout exception thrown when socket operation takes more than 5 seconds; for weak/long distance conns, increase; 5sec fine for localhost practice
+  conn.settimeout(5) 
   try: 
     while True:
       data = conn.recv(4096)
@@ -53,6 +54,59 @@ def recv_from(conn):
   except Exception as e:
     return buffer
   return buffer
+
+
+# Modify request packs transporting through proxy prior to forwarding
+def request_handler(buff):
+  #TODO
+  return buff
+
+
+# Modify response packs transporting through proxy prior to forwarding
+def response_handler(buff):
+  #TODO
+  return buff
+
+
+def proxy_handler(client_sock, remote_host, remote_port, receive_first):
+  remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # establish conn to remote host (cont' below)
+  remote_socket.connect((remote_host, remote_port)) # connect with supplied (IP, PORT) params
+
+  # for some connection types, we may need to initiate connection to remote and req data before continuing. Check this here based on param
+  if receive_first:
+    remote_buff = receive_from(remote_socket) # receive data from remote socket, store in buffer
+    hexdump(remote_buff) # get data from packet received, then continue
+
+  remote_buff = response_handler(remote_buff) # pass in original remote buffer and update it with modified buffer returned from fct
+  if len(remote_buff): # something exists in buffer...
+    client_socket.send(remote_buff)
+
+  while True: 
+    client_buff = receive_from(client_sock)
+    # if we received something from client, print stats
+    if len(client_buff):
+      print(f"{len(client_buff)} bytes recceived from client")
+      hexdump(client_buff)
+      client_buff = request_handler(client_buff)
+      remote_socket.send(client_buff)
+      print("Data sent to remote")
+
+  # after sending data, wait for response from remote host and call handler function for response received
+    remote_buff = receive_from(remote_socket)
+    if len(remote_buff): # check if we've received anything before continuing
+      print("{} bytes received from remote host".format(len(remote_buff)))
+      hexdump(remote_buff) # print some stats
+      remote_buff = response_handler(remote_buff) # call handler to modify
+      client_sock.send(remote_buff)
+      print("Response sent to client")
+
+    if not len(remote_buff) or not len(client_buff): # nothing more to do, exit loop
+      client_sock.close()
+      remote_socket.close()
+      break
+  return
+      
+
   
   
     
