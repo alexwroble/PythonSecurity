feee2dac182a187e75ef6f2001a6612e12ecfe13
Alex Wroble <alexwroble@gmail.com>
2025-01-07 16:32:12 -0500

    Created fileTransferAttacker.py

---
+++ fileTransferAttacker.py
@@ -0,0 +1,21 @@
+# fileTransferAttacker.py implements a variety of attacks against the file transfer
+# system created with requester.py and sender.py
+
+import argparse
+import socket
+import sys
+import textwrap
+
+def main(args):
+    # TODO
+    exit(0)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Distributed File Transfer System Attacker", epilog=textwrap.dedent("""
+        Examples:
+            python3 fileTransferAttacker.py _____add stuff later______
+"""))
+    main()
